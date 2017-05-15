94357a5c8661e5231bf5bd5add7c29f5c17a67d5
PaulProS <iamlearning13@gmail.com>
2017-05-15 14:59:41 +0530

    Login and Register forms CSS issue fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,79 +7,41 @@
             {{--<div class="panel panel-default">--}}
                 {{--<div class="panel-heading">Login</div>--}}
                 {{--<div class="panel-body">--}}
-
-
-{{--<form action="#" method="post">--}}
-    {{--<h3>LOGIN</h3>--}}
-    {{--<input type="text" Name="Userame" placeholder="Username" required="">--}}
-    {{--<input type="password" Name="Password" placeholder="Password" required="">--}}
-    {{--<ul class="tick w3layouts agileinfo">--}}
-        {{--<li>--}}
-            {{--<input type="checkbox" id="brand1" value="">--}}
-            {{--<label for="brand1"><span></span>Remember me</label>--}}
-        {{--</li>--}}
-        {{--<li>--}}
-            {{--<a href="#">Forgot Password?</a>--}}
-        {{--</li>--}}
-    {{--</ul>--}}
-    {{--<div class="send-button wthree agileits">--}}
-        {{--<input type="submit" value="LOGIN">--}}
-    {{--</div>--}}
-{{--</form>--}}
-
-
-
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <input id="email" type="email" class="email aitsw3ls" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" name="password" placeholder="Password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
+                        <ul class="tick w3layouts agileinfo">
+                            <li>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label for="brand1"><span></span>Remember me</label>
+                            </li>
+                            <li>
+                                <a href="{{ route('password.request') }}">Forgot Password?</a>
+                            </li>
+                        </ul>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+                        <div class="send-button wthree agileits">
+                            <input type="submit" value="LOGIN">
                         </div>
                     </form>
                 {{--</div>--}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,80 +7,46 @@
             {{--<div class="panel panel-default">--}}
                 {{--<div class="panel-heading">Register</div>--}}
                 {{--<div class="panel-body">--}}
-
-
-{{--<form action="#" method="post">--}}
-    {{--<h3>SIGN UP</h3>--}}
-    {{--<input type="text" Name="Name" placeholder="Name" required="">--}}
-    {{--<input type="text" Name="Email" placeholder="Email" required="">--}}
-    {{--<input type="password" Name="Password" placeholder="Password" required="">--}}
-    {{--<input type="text" Name="Phone Number" placeholder="Phone Number" required="">--}}
-    {{--<div class="send-button wthree agileits">--}}
-        {{--<input type="submit" value="SIGN UP">--}}
-    {{--</div>--}}
-{{--</form>--}}
-
-
-
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
+                        <h3>SIGN UP</h3>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                        <div class="{{ $errors->has('name') ? ' has-error' : '' }}">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" name="name" placeholder="Name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="{{ $errors->has('email') ? ' has-error' : '' }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" class="email aitsw3ls" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                        <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
+                             <input id="password" type="password" name="password" placeholder="Password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                             <input id="password-confirm" type="password" name="password_confirmation" placeholder="Confirm Password" required>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
+                        <div class="send-button wthree agileits">
+                            <input type="submit" value="SIGN UP">
                         </div>
                     </form>
                 {{--</div>--}}
