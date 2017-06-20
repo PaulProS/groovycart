6b2cd5eed5d9ef72889e2141142e4c9966ec5169
PaulProS <iamlearning13@gmail.com>
2017-06-20 11:56:54 +0530

    Menu UI Fixes

--- resources/views/layouts/bestStore/header.blade.php
+++ resources/views/layouts/bestStore/header.blade.php
@@ -3,7 +3,7 @@
 <div class="header">
     <div class="container">
         <div class="header-grid">
-            <div class="header-grid-left animated wow slideInLeft" data-wow-delay=".5s">
+            <div class="header-grid-left">
                 <ul>
                     <li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:sales@beststore.com">sales@beststore.com</a></li>
                     <li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>+91 987 525 4477</li>
@@ -41,7 +41,7 @@
                     @endif
                 </ul>
             </div>
-            <div class="header-grid-right animated wow slideInRight" data-wow-delay=".5s">
+            <div class="header-grid-right">
                 <ul class="social-icons">
                     <li><a href="https://www.facebook.com/" target="_blank" class="facebook"></a></li>
                     <li><a href="https://twitter.com/" target="_blank" class="twitter"></a></li>
@@ -52,7 +52,7 @@
             <div class="clearfix"> </div>
         </div>
         <div class="logo-nav">
-            <div class="logo-nav-left animated wow zoomIn" data-wow-delay=".5s">
+            <div class="logo-nav-left">
                 <h1><a href="/">Best Store <span>Shop anywhere</span></a></h1>
             </div>
             <div class="logo-nav-left1">
@@ -71,48 +71,10 @@
                             <li class="active"><a href="/" class="act">Home</a></li>
                             <!-- Mega Menu -->
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <b class="caret"></b></a>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Categories<b class="caret"></b></a>
                                 <ul class="dropdown-menu multi-column columns-3">
                                     <div class="row">
                                         @include('layouts.bestStore.partials.menu')
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </ul>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Furniture <b class="caret"></b></a>
-                                <ul class="dropdown-menu multi-column columns-3">
-                                    <div class="row">
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Home Collection</h6>
-                                                <li><a href="#">Cookware</a></li>
-                                                <li><a href="#">Sofas</a></li>
-                                                <li><a href="#">Dining Tables</a></li>
-                                                <li><a href="#">Shoe Racks</a></li>
-                                                <li><a href="#">Home Decor</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Office Collection</h6>
-                                                <li><a href="#">Carpets</a></li>
-                                                <li><a href="#">Tables</a></li>
-                                                <li><a href="#">Sofas</a></li>
-                                                <li><a href="#">Shoe Racks</a></li>
-                                                <li><a href="#">Sockets</a></li>
-                                                <li><a href="#">Electrical Machines</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Decorations</h6>
-                                                <li><a href="#">Toys</a></li>
-                                                <li><a href="#">Wall Clock</a></li>
-                                                <li><a href="#">Lighting</a></li>
-                                                <li><a href="#">Top Brands</a></li>
-                                            </ul>
-                                        </div>
                                         <div class="clearfix"></div>
                                     </div>
                                 </ul>
